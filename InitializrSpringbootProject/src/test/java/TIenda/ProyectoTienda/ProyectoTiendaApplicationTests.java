22dce6bd311b8ae5702deab4363e3b0f951db2fc
HumbleDiligents <sebastiancascantecc27@gmail.com>
2023-01-22 14:43:54 -0600

    Clase_1

---
+++ InitializrSpringbootProject/src/test/java/TIenda/ProyectoTienda/ProyectoTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package TIenda.ProyectoTienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
